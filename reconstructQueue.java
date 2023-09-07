b6c56fc9a5e9b05d1af235d9c45d787acc1f47f9
Prem Kumar Amanchi <premkumaramanchi05@gmail.com>
2023-09-06 19:18:07 -0700

    add 2 files

--- reconstructQueue.java
+++ reconstructQueue.java
@@ -1,25 +1,22 @@
 import java.util.*;
-// tc : O(n)
-// sc : O(1) - maximum you would store only 26 alphabets
+
+// tc : O(nlog(n))
+// sc : O(1) - not assuming the output size
 
 class Solution {
-    public int leastInterval(char[] tasks, int n) {
-        int max=0, maxCount=0;
-        int N = tasks.length ; 
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char task : tasks){
-            map.put(task,map.getOrDefault(task,0)+1);
-            max = Math.max(max, map.get(task));
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new peopleComaparator());
+        List<int[]> result = new ArrayList<>();
+        for(int[] pep : people)
+            result.add(pep[1],pep);
+        return result.toArray(new int[people.length][2]);
+    }
+
+    public class peopleComaparator implements Comparator<int[]> {
+        public int compare(int[]a, int[]b){
+            if(a[0]==b[0])
+                return a[1]-b[1];
+            return b[0]-a[0];
         }
-        
-        for(char task : map.keySet())
-            if(max == map.get(task))
-                maxCount++;
-        
-        int partitions = max-1;
-        int pendingingTasks = N - (max*maxCount);
-        int emptySlots = partitions * (n-maxCount+1);
-        int idle = Math.max(0,emptySlots-pendingingTasks);
-        return N+idle;
     }
 }
