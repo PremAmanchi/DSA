59b643f27c725972ab34548bcb6e96b215713511
Prem Kumar Amanchi <101784948+PremAmanchi@users.noreply.github.com>
2023-10-23 16:10:18 -0700

    Create containsDuplicate.java

---
+++ containsDuplicate.java
@@ -0,0 +1,15 @@
+// tc : O(n)
+// sc : O(n)
+// problem link : https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num))
+                return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
